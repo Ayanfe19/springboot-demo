82cd4596a7835da19653dd40fb4bba1e67fd7b49
Oluwafemi Aina <OLUAAINA@unionbankng.com>
2023-03-15 17:06:32 +0100

    Person Controller

--- src/main/java/com/example/demo/dao/FakePersonDataAcessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAcessService.java
@@ -1,9 +1,14 @@
-package com.example.demo.model;
+package com.example.demo.dao;
 
 import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+@Repository
 public class FakePersonDataAcessService implements PersonDao {
     private static list<Person> DB = new ArrayList<>();
 
