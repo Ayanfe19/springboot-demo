52f4ef6d2834cf1be442de2983faa9216a439def
Oluwafemi Aina <oluwafemi.wafi@gmail.com>
2023-03-18 15:36:09 +0100

    Bug Fixed

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -2,7 +2,7 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.contraints.NotBlank;
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 public class Person {
